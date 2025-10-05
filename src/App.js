d1aeea273709ac4b67e6b51001fe55ee523e458a
jahanzeb-mughal <hassanjahanzeb8@gmail.com>
2025-10-05 22:35:39 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [currDeviceTime, setCurrDeviceTime] = useState()
+  const [currDeviceDate, setCurrDeviceDate] = useState('')
+  const [visitTime, setVisitTime] = useState('')
+  const [visitDate, setVisitDate] = useState('')
+  const [time, setTime] = useState('')
+
+  useEffect(() => {
+    // Get current date and time
+    const now = new Date();
+
+    // Format date to YYYY-MM-DD
+    const date = now.toISOString().split('T')[0];
+
+    // Format time to HH:MM (24-hour format)
+    const time = now.toTimeString().slice(0, 5);
+
+    setCurrDeviceDate(date);
+    setCurrDeviceTime(time);
+    setVisitDate(date)
+    setVisitTime(time)
+  }, []);
+
+  const timeDiff = () => {
+  const deviceNow = new Date(`${currDeviceDate}T${currDeviceTime}`); 
+  const targetTime = new Date(`${visitDate}T${visitTime}`); 
+  const currTime = new Date(); 
+
+  const diffrence = currTime - deviceNow;
+  const deviceTargetTime = new Date(targetTime.getTime() - diffrence);
+
+  const readable = deviceTargetTime.toLocaleString();
+
+  setTime(`Time ${readable}`);
+};
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>Faulty Clock</h1>
+        <div>
+          <h2>Enter Device Current Time</h2>
+          <input type="time" value={currDeviceTime} onChange={(e) => setCurrDeviceTime(e.target.value)} />
+          <input type="date" value={currDeviceDate} onChange={(e) => setCurrDeviceDate(e.target.value)} />
+        </div>
+
+        <div>
+          <h2>Enter time you want to visit</h2>
+          <input type="time" value={visitTime} onChange={(e) => setVisitTime(e.target.value)} />
+          <input type="date" value={visitDate} onChange={(e) => setVisitDate(e.target.value)} />
+        </div>
+
+        <button onClick={timeDiff}>Check Time</button>
+        <p>{time}</p>
+      </div>
     </div>
   );
 }
